e8f24938342de3cff12a6d9201fe617c83e0eef5
edupedu101 <edupedu101.eduardvd2001@hotmail.com>
2022-05-02 14:34:39 +0000

    pagina registros creada

--- AlertZ/settings.py
+++ AlertZ/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'AlertZ.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
