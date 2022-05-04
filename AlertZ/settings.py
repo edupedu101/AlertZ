3cb21a76c831068be8043242455d878e42b817b2
edupedu101 <edupedu101.eduardvd2001@hotmail.com>
2022-05-04 15:46:30 +0000

    login hecho

--- AlertZ/settings.py
+++ AlertZ/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'AlertZ.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': [BASE_DIR / 'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,11 +134,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login Info
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login'
